28e2925a175486506603a888cf41bafb147e1f16
Saeed Mahmud <119888472+xsayedmahmud@users.noreply.github.com>
2023-03-21 00:21:24 +0300

    Add click and mouseover drag listener

--- app.js
+++ app.js
@@ -1,20 +1,66 @@
-const sketchbox = document.querySelector("#sketchbox");
+const sketchBox = document.querySelector("#sketchbox");
 const slider = document.querySelector("#slider");
+const blockNumbers = document.querySelector(".block-num");
+let mousePressed = false;
 
 function updateSketchBox() {
   const value = slider.value;
-  sketchbox.style.setProperty("--value", value);
+  sketchBox.style.setProperty("--value", value);
 
-  while (sketchbox.firstChild) {
-    sketchbox.removeChild(sketchbox.firstChild);
+  while (sketchBox.firstChild) {
+    sketchBox.removeChild(sketchBox.firstChild);
   }
 
   for (let i = 0; i < value * value; i++) {
     const sketchBlock = document.createElement("div");
     sketchBlock.classList.add("skb");
-    sketchbox.appendChild(sketchBlock);
+    sketchBox.appendChild(sketchBlock);
+    blockNumbers.textContent = `${value} x ${value}`;
+    blockNumbers.style.setProperty("font-size", "1.5`rem");
   }
 }
 
 updateSketchBox();
 slider.addEventListener("input", updateSketchBox);
+
+// function changeColor(e) {
+//   sketchBox.addEventListener("mouseup", () => {
+//     mousePressed = false;
+//   });
+//   sketchBox.addEventListener("mousedown", () => {
+//     mousePressed = true;
+//   });
+//   if (mousePressed && e.target.classList.contains("skb")) {
+//     e.target.style.backgroundColor = "black";
+//     e.stopPropagation();
+//   }
+// }
+// sketchBox.addEventListener("mouseover", changeColor);
+
+// sketchBox.addEventListener("click", (e) => {
+//   if (e.target.classList.contains("skb")) {
+//     e.target.style.backgroundColor = "black";
+//     e.stopPropagation();
+//   }
+// });
+
+function changeColor(e) {
+  if (e.type === "mouseup") {
+    mousePressed = false;
+  } else if (e.type === "mousedown") {
+    mousePressed = true;
+  } else if (e.type === "mouseover" || e.type === "click") {
+    if (
+      mousePressed ||
+      (e.type === "click" && e.target.classList.contains("skb"))
+    ) {
+      e.target.style.backgroundColor = "black";
+      e.stopPropagation();
+    }
+  }
+}
+
+sketchBox.addEventListener("mouseup", changeColor);
+sketchBox.addEventListener("mousedown", changeColor);
+sketchBox.addEventListener("mouseover", changeColor);
+sketchBox.addEventListener("click", changeColor);
